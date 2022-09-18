4f38c26a283f32177c6d4af901bf4450fe927299
nahalhz <113655682+nahalhz@users.noreply.github.com>
2022-09-18 19:22:16 -0400

    Update memory tile game.py

--- memory tile game.py
+++ memory tile game.py
@@ -1,16 +1,6 @@
 #----------------------------------------------------------------------------
-# Program: SUMMATIVE: MEMORY TILE GAME
+# Program: MEMORY TILE GAME
 # Author: Nahal H.
-# Date: December 23, 2020
-# Description: The program lets the users choose between three categories with 
-# different difficulties which then randomly chooses a picture rebus puzzle for a
-# multiplayer game of memory tiles under which the puzzle is hidden. The first player who
-# is able to guess the puzzle is the winner. 
-# They can quit the game and go back to categories menu to try another category 
-# Input: User uses the cruisor to input coordinates and therefore allow
-# them to press on the cards and so program to function accordingly based
-# on the instruction that follow. They are also able to type of their guess
-# in the input box that pops up to indicate whether they got it correctly or not. 
 #---------------------------------------------------------------------------
 #------------------------#
 #    IMPORTING MODULES   #
